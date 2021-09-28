ec70cab5d4eea9cdfc6e4adeded589c1f28e1836
atomicstorm <59619595+atomicstorm-hca@users.noreply.github.com>
2021-09-28 11:06:10 -0500

    change ip/port

--- nft-manager/config/server.js
+++ nft-manager/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '127.0.0.1'),
+  port: env.int('PORT', 5000),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '003a66e585c43a13eb3869d888415f53'),
